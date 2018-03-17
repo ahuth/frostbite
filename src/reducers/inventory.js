d53a8ab2cad165fe2d96fc884a67a34935685531
Andrew Huth <ahuth@mavenlink.com>
2018-03-17 10:03:16 -0700

    Revert "Use a constant for action names"
    This reverts commit 9b88611451dae742e501c8865cbca98cb7dd3295.

--- src/reducers/inventory.js
+++ src/reducers/inventory.js
@@ -2,11 +2,9 @@ const initialState = {
   wood: 0,
 };
 
-const WOOD_GATHERED = 'WOOD_GATHERED';
-
 export default function reducer(state = initialState, action) {
   switch (action.type) {
-    case WOOD_GATHERED:
+    case 'WOOD_GATHERED':
       return Object.assign({}, state, { wood: state.wood + action.payload.amount });
     default:
       return state;
@@ -15,7 +13,7 @@ export default function reducer(state = initialState, action) {
 
 export function gatherWood(amount) {
   return {
-    type: WOOD_GATHERED,
+    type: 'WOOD_GATHERED',
     payload: { amount },
   };
 }
