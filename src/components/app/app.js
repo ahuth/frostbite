00cd928bd3a563e8ba278c8eacd7c453a181bdcc
Andrew Huth <ahuth@mavenlink.com>
2018-03-16 21:04:40 -0700

    Move App into a directory and style with fonts

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './app.css';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <span>hello world</span>
       </div>
     );
