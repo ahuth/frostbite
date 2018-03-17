6f9c8936ce1f91b21c11ea4cefe99fd6e99d7dd7
Andrew Huth <ahuth@mavenlink.com>
2018-03-16 21:09:01 -0700

    Add an "action"

---
+++ src/components/action/action.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './action.css';
+
+export default function Action({ text }) {
+  return <button className="Action">{text}</button>;
+}
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Action from '../action';
 import './app.css';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <span>hello world</span>
+        <Action text="Gather wood" />
       </div>
     );
   }
