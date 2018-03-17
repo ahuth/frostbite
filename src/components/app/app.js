f39523858b3c0dc03e1413fc535ffb8cfa83e9dc
Andrew Huth <ahuth@mavenlink.com>
2018-03-16 21:33:45 -0700

    Add component to execute a callback on an interval

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Action from '../action';
 import A11A11yIntention from '../a11y-intention';
+import Timer from '../timer';
 import './app.css';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Timer interval={1500} callback={time => console.log(time)} />
         <A11A11yIntention>
           <Action text="Gather wood" />
         </A11A11yIntention>
