bc72978f35739cf1305f58acaf7911b3b01632d8
Andrew Huth <andrew@huth.me>
2019-07-31 22:55:13 -0700

    Add hut building action

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,12 +3,14 @@ import Action from './Action';
 import Inventory from './Inventory';
 
 export default function App() {
+  const [huts, setHuts] = useState(0);
   const [wood, setWood] = useState(0);
 
   return (
     <div>
       <Action cooldown={2000} onClick={() => setWood(w => w + 1)} text="Gather wood" />
-      <Inventory huts={0} wood={wood} />
+      <Action cooldown={5000} onClick={() => setHuts(h => h + 1)} text="Build hut" />
+      <Inventory huts={huts} wood={wood} />
     </div>
   );
 }
