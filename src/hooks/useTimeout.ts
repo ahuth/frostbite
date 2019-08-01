410f8f9696e5b16b34ed871c6ae3f8568e5377fa
Andrew Huth <andrew@huth.me>
2019-08-01 16:42:47 -0700

    Speeling

--- src/hooks/useTimeout.ts
+++ src/hooks/useTimeout.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react';
 
-export default function useTImeout(callback: () => void, timeout: number, active = false) {
+export default function useTimeout(callback: () => void, timeout: number, active = false) {
   const callbackRef = useRef(callback);
 
   useEffect(() => {
