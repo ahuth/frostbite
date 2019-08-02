7c5230ace077f37a79dd14b3c60e89ca0a90b96c
Andrew Huth <andrew@huth.me>
2019-08-01 21:45:55 -0700

    Updates

---
+++ precache-manifest.b8f80099d78e3e2dc3fc86215bbd1280.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfa2a771b9d9f6553979af7b89aec283",
+    "url": "/frostbite/index.html"
+  },
+  {
+    "revision": "4d82334600dee4ff24e1",
+    "url": "/frostbite/static/css/main.ce9e6b8a.chunk.css"
+  },
+  {
+    "revision": "972e1d5a1242189d55c8",
+    "url": "/frostbite/static/js/2.abeba369.chunk.js"
+  },
+  {
+    "revision": "4d82334600dee4ff24e1",
+    "url": "/frostbite/static/js/main.39702a25.chunk.js"
+  },
+  {
+    "revision": "05f1526017ba58c8fce7",
+    "url": "/frostbite/static/js/runtime~main.61aa38ef.js"
+  }
+]);
